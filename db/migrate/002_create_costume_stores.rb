0c7d958e20338b9a22f266acbdec7a5a03bee022
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-26 00:17:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location
-      t.integer :inventory
-      t.integer :employee_count
-      t.boolean :open_closed
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
       
